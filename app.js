4aeeced0560c9a25c2906f1065c7dcf264c68276
Gabriel <gabriel.schwartzmann@free.fr>
2024-10-28 15:19:43 +0100

    added routes and model for posts

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 const cors = require('cors');
@@ -17,6 +18,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 module.exports = app;
