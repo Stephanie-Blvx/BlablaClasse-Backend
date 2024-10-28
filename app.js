2d20867c1b227deb099a294981ebf712be6754a3
Nahy <nahy.elearning@gmail.com>
2024-10-28 14:21:20 +0100

    major : create model schema kids, teachers and parents; create routes kids, teachers and parents

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require('dotenv').config();
 var express = require('express');
+require('./models/connection');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var parentsRouter = require('./routes/parents');
+var teachersRouter = require('./routes/teachers');
+var kidsRouter = require('./routes/kids');
 
 var app = express();
 const cors = require('cors');
@@ -17,6 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/parents', parentsRouter);
+app.use('/teachers', teachersRouter);
+app.use('/kids', kidsRouter);
 
 module.exports = app;
---
+++ models/parents.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const parentSchema = mongoose.Schema({
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+    token: String,
+    children: [{ type: mongoose.Schema.Types.ObjectId, ref: 'kids' }], //plusieurs enfants possibles à priori donc tableau d'id kids
+   });
+
+
+const Parent = mongoose.model('parents', parentSchema);
+
+module.exports = Parent;
