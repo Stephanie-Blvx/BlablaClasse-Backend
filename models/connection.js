d93952774f2ff988a22468aedcf45e61ed74ee5d
stéphanie <sbelveaux@gmail.com>
2024-10-28 15:24:05 +0100

    back

--- models/connection.js
+++ models/connection.js
@@ -4,8 +4,4 @@ const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log('Database connected'))
-<<<<<<< HEAD
   .catch(error => console.error(error));
-=======
-  .catch(error => console.error(error));
->>>>>>> 1722a0ac4e00823bf71975cb60d3d2291bf5edcd
