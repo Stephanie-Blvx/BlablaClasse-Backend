97d8a6a84feba5f808ffde029490979d36dc67ad
Marina <ma.po@hotmail.fr>
2024-11-05 17:01:42 +0100

    Update teachers.js
    effacé console.log en doublon

--- routes/teachers.js
+++ routes/teachers.js
@@ -96,8 +96,7 @@ router.post("/signintoken", (req, res) => {
   Teacher.findOne({ token: req.body.token }) // Vérifie si le token existe dans la Collection Teachers
     // .populate('classes') //peuple le champ classes
     .then((data) => {
-      console.log("data retournée par la database:----------->", data);  // console.log la réponse de la database
-      console.log("data retournée par la database:----------->", data);  // console.log la réponse de la database
+          //console.log("data retournée par la database:----------->", data);  // console.log la réponse de la database
     if (data) {
       res.json({
         result: true,
